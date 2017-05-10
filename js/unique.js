7b34fac8f1d97c1bac565be5642711bae48d7cf1
Tiger Oakes <tigeroakes@gmail.com>
2017-05-09 19:07:17 -1000

    Finalizing merge

--- js/unique.js
+++
@@ -1,9 +0,0 @@
-function uniqueCharacters(...strings) {
-	let unique = '';
-	for (const string of strings) {
-		for (const char of string) {
-			if (!unique.includes(char)) unique += char;
-		}
-	}
-	return unique;
-}
